e8a768611415bdde8b5ef6098fe4a8fa67aebde2
Revanth Tadikonda <BaTmAn@Revanths-MacBook-Air.local>
2017-07-11 14:57:23 +0530

    Add GET/todos route

--- server/server.js
+++ server/server.js
@@ -25,6 +25,15 @@ app.post('/todos',(req,res)=>{
     })
 });
 
+// This is used to return all the todos. Any todo app will require a get Todo as the first screen you will show the user is the list of all the todos
+app.get('/todos',(req,res)=>{
+   Todo.find().then((todos)=>{
+        res.send({todos});
+   },(e)=>{
+        res.status(400).send(e);
+   })
+});
+
 app.listen(3000,()=>{
     console.log('Started on port 3000');
 });
